5c26d047c4d117af4d49ff1c8e2b8df9417c9696
Francis Bourassa <francis@ascense.ca>
2019-03-03 15:49:33 -0500

    Disabled syncing temporarly

--- src/renderer/sync_utils.js
+++ src/renderer/sync_utils.js
@@ -12,98 +12,98 @@ export default {
    * We also check when the application was just started.
    */
   sync_results() {
-    var syncUrl = global.__settings.sync_results_url;
-    var syncVersion = global.__settings.sync_api_version;
-    var token = store.state.syncApiToken;
+    // var syncUrl = global.__settings.sync_results_url;
+    // var syncVersion = global.__settings.sync_api_version;
+    // var token = store.state.syncApiToken;
 
-    if (syncUrl && syncVersion && token) {
-      // Flag to stop process on error
-      var interruptSync = false;
+    // if (syncUrl && syncVersion && token) {
+    //   // Flag to stop process on error
+    //   var interruptSync = false;
       
-      // Used to limit requests to one at a time
-      var q = queue({ concurrency: 1, autostart: true });
-      q.on('timeout', function (next, job) { next(); });
-      q.on('end', function (result, job) {
-        store.commit('setSyncPercent', null);
-      })
+    //   // Used to limit requests to one at a time
+    //   var q = queue({ concurrency: 1, autostart: true });
+    //   q.on('timeout', function (next, job) { next(); });
+    //   q.on('end', function (result, job) {
+    //     store.commit('setSyncPercent', null);
+    //   })
 
-      // Find results wich were not synced yet..
-      var results = resultsDb.find({
-        $or: [
-          {'sync.version': { $ne: syncVersion }},
-          {'sync.synced': { $ne: true }}
-        ]
-      }).exec(function (err, results) {
-        console.log("Results to sync", results);
+    //   // Find results wich were not synced yet..
+    //   var results = resultsDb.find({
+    //     $or: [
+    //       {'sync.version': { $ne: syncVersion }},
+    //       {'sync.synced': { $ne: true }}
+    //     ]
+    //   }).exec(function (err, results) {
+    //     console.log("Results to sync", results);
 
-        if (err) console.error(err);
-        else {
-          // Group by chunks of 100 to minimise requests
-          var total = results.length;
-          var completed = 0;
-          var resultsChunks = arrayChunk(results, 100);
+    //     if (err) console.error(err);
+    //     else {
+    //       // Group by chunks of 100 to minimise requests
+    //       var total = results.length;
+    //       var completed = 0;
+    //       var resultsChunks = arrayChunk(results, 100);
 
-          for (var i = 0; i < resultsChunks.length; i++) {
-            let chunk = resultsChunks[i];
-            let chunkData = chunk.map(result => {
-              return {
-                'is_medal': result.isMedal,
-                'rank': result.rank,
-                'precision': result.precision,
-                'date': result.date,
-                'user_id': result.user.channelId,
-                'user_display_name': result.user.displayName, 
-                'user_profile_image_url': result.user.profileImageUrl,
-                'comment_id': result.id
-              }
-            });
+    //       for (var i = 0; i < resultsChunks.length; i++) {
+    //         let chunk = resultsChunks[i];
+    //         let chunkData = chunk.map(result => {
+    //           return {
+    //             'is_medal': result.isMedal,
+    //             'rank': result.rank,
+    //             'precision': result.precision,
+    //             'date': result.date,
+    //             'user_id': result.user.channelId,
+    //             'user_display_name': result.user.displayName, 
+    //             'user_profile_image_url': result.user.profileImageUrl,
+    //             'comment_id': result.id
+    //           }
+    //         });
 
-            q.push(function (next) {
-              if (interruptSync) return next();
+    //         q.push(function (next) {
+    //           if (interruptSync) return next();
 
-              request({ 
-                method: 'POST',
-                url: syncUrl,
-                form: { 
-                  access_token: token,
-                  sync_version: syncVersion,
-                  results_json: JSON.stringify(chunkData)
-                }
-              }, function (error, response, body) {
-                if (error) { 
-                  interruptSync = true;
-                  next();
-                } else if (response.statusCode === 403) {
-                  interruptSync = true;
-                  // Invalid token, unset...
-                  $.notify("Invalid sync ID.", 'error');
-                  store.commit('setSyncApiToken', null);
-                  exportTokenStorage();
-                  next();
-                } else if (response.statusCode !== 200) {
-                  console.error(body);
-                  next();
-                } else {
-                  // Update sync status in the database
-                  let ids = chunk.map(result => result.id);
-                  resultsDb.update({ id: { $in: ids }}, {
-                    $set: { 
-                      "sync.version": syncVersion, 
-                      "sync.synced": true 
-                    } 
-                  }, { multi: true }, function () {
-                    completed += chunk.length;
-                    store.commit('setSyncPercent', completed / total);
-                    console.log(`Successfully synced ${chunk.length} results.`);
-                    next();
-                  });
-                }
-              });
-            });
-          }
+    //           request({ 
+    //             method: 'POST',
+    //             url: syncUrl,
+    //             form: { 
+    //               access_token: token,
+    //               sync_version: syncVersion,
+    //               results_json: JSON.stringify(chunkData)
+    //             }
+    //           }, function (error, response, body) {
+    //             if (error) { 
+    //               interruptSync = true;
+    //               next();
+    //             } else if (response.statusCode === 403) {
+    //               interruptSync = true;
+    //               // Invalid token, unset...
+    //               $.notify("Invalid sync ID.", 'error');
+    //               store.commit('setSyncApiToken', null);
+    //               exportTokenStorage();
+    //               next();
+    //             } else if (response.statusCode !== 200) {
+    //               console.error(body);
+    //               next();
+    //             } else {
+    //               // Update sync status in the database
+    //               let ids = chunk.map(result => result.id);
+    //               resultsDb.update({ id: { $in: ids }}, {
+    //                 $set: { 
+    //                   "sync.version": syncVersion, 
+    //                   "sync.synced": true 
+    //                 } 
+    //               }, { multi: true }, function () {
+    //                 completed += chunk.length;
+    //                 store.commit('setSyncPercent', completed / total);
+    //                 console.log(`Successfully synced ${chunk.length} results.`);
+    //                 next();
+    //               });
+    //             }
+    //           });
+    //         });
+    //       }
           
-        }
-      });
-    }
+    //     }
+    //   });
+    // }
   }
 };
